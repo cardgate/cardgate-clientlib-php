140e34370e018fafc71622d4bb67d783c2847338
richard <richard@cardgate.com>
2023-03-01 13:00:24 +0100

    New: cardgate generic module.

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once dirname( __FILE__ ) . '/src/Autoloader.php';
+
+cardgate\api\Autoloader::register();
--- tests/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once dirname( __FILE__ ) . '/../src/Autoloader.php';
-
-cardgate\api\Autoloader::register();
